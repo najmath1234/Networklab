d1bf695fd3521082767199b4cf984f7a713e4093
Najuhilal <najumv36@gmail.com>
2025-02-20 11:54:33 +0530

    Add files via upload

---
+++ week 2/file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "hello world"
+var=2
+echo "$var"
+((sum=35 + 25 ))
+echo "$sum"
+echo "what is your name"
+read name
+echo "my name is $name"
+echo "total arguments:$#"
+echo "first arguments:$1"
+echo "second arguments:$0"
