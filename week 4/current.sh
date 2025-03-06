9f9b260ce804d658f22970030ca50c3c0d58d1b4
Najuhilal <najumv36@gmail.com>
2025-03-06 11:01:42 +0530

    Add files via upload

---
+++ week 4/current.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+current_datetime=$(date)
+
+username=$(whoami)
+
+current_dir=$(pwd)
+
+echo "Current Date and Time: $current_datetime"
+echo "Username: $username"
+echo "Current Working Directory: $current_dir"
+
